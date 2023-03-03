99cc48c25d5944d294c8a162731ffecb12f3af57
YCWang <yc@wakeup.com.tw>
2023-03-03 11:12:59 +0800

    feat: docker-sync

--- up.sh
+++ up.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker-compose up -d mariadb redis php-fpm nginx workspace apache2 php-fpm80 php-fpm74
+# docker-compose up -d mariadb redis php-fpm nginx workspace apache2 php-fpm74
+./sync.sh up -d mariadb redis php-fpm nginx workspace apache2 php-fpm74
